2b405bc749d1ce5c8337b137ac73828b8101615a
Yujun Cho <devhante@gmail.com>
2023-09-29 11:12:07 +0900

    20230905_2884

--- 20230905_2884.cpp
+++ 20230905_2884.cpp
@@ -27,6 +27,22 @@ int main()
 {
     int h, m;
     cin >> h >> m;
-    cout << h << endl;
-    cout << m << endl;
+
+    if (m >= 45)
+    {
+        m -= 45;
+    }
+    else
+    {
+        m = 60 - (45 - m);
+        if (h != 0)
+        {
+            h -= 1;
+        }
+        else
+        {
+            h = 23;
+        }
+    }
+    cout << h << " " << m;
 }
